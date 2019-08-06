907e5373eccf53f679818c198c9728433b0e57b7
Siavash Tabrizian <30736084+siavashtab@users.noreply.github.com>
2019-08-05 20:21:23 -0500

    Update RV_structs.h

--- src/RV_structs.h
+++ src/RV_structs.h
@@ -38,36 +38,4 @@ struct SSample
 	double UB;
 };
 
-struct Clusters_SSample
-{
-	int mainscen;                       //the main scenario
-	int size;                           //number of scenarios in the cluster
-	std::vector<SSample> samples;    //all sample info in the clusters
-	double weight;
 
-	double UB;                          //UB of the cluster
-	vec_d  sub_UB;                      //all UB of scenarios in the cluster
-
-	double within_cluster_square;
-	int new_partition_num;              //number of new partions this sample found               
-	bool NewPartitionsFound;
-	bool Active;
-	int SubScenDepth;
-
-	double pi_var;
-	double sample_var;
-
-	double rpi;          //the term sum(r . pi - sum( r . pi ) )
-	double Cpi;          //the term sum(C . pi . x - sum( C . pi . x ) )
-};
-
-struct Partitions
-{
-	double   pi_norm;
-	vec_d    pi;
-	vec_i    scens;           //scenarios inside this partition in the last iteration
-	vec_i    max_extreme_scens;
-	vec_i    min_extreme_scens;
-	vec_i    iter;            //iterations that this partition found
-	bool    isActive;         //is the pi is important in the LShaped cut
-};
